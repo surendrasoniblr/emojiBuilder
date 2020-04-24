b2e41498ac8d93d8322df8c1ba1b34bd047c3dde
patrickLabes <patrick.labes@digistorm.com.au>
2020-04-24 14:56:39 +1000

    finished letters and deployed

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,15 +2,15 @@
   <div>
     <nav class="navbar header has-shadow is-primary" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
-        <a class="navbar-item" href="/">
+        <!-- <a class="navbar-item" href="/">
           <img src="~assets/buefy.png" alt="Buefy" height="28" />
-        </a>
+        </a> -->
 
-        <div class="navbar-burger">
+        <!-- <div class="navbar-burger">
           <span />
           <span />
           <span />
-        </div>
+        </div> -->
       </div>
     </nav>
 
